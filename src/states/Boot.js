fe60fdc5ddf43ede596540a5cc2a87c863e4648c
aurimas-darguzis <aurimas@darguzis.com>
2017-12-05 22:44:03 +0000

    update

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -19,8 +19,25 @@ export default class extends Phaser.State {
     let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
     text.anchor.setTo(0.5, 0.5)
 
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
+    this.load.image('loaderBg', '../../assets/images/loading_bg.jpg')
+    this.load.image('loaderBar', '../../assets/images/loading_bar.png')
+
+    /**
+     * Sprites for animated player and mouse, which have some animations and images for backgorund and foreground.
+     * The map data for 2 levels, using tiled_json format.
+     * Images - signs, collectables, UI elements.
+     * Audio sprite
+     */
+    this.load.spritesheet('player', '../../assets/images/sprites/FoxSprite.png', 210, 210)
+    this.load.image('gameBg', '../../assets/images/Background.png')
+    this.load.tilemap('level1', '../../assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON)
+    this.load.tilemap('level2', '../../assets/levels/level2.json', null, Phaser.Tilemap.TILED_JSON)
+    this.load.image('Tiles', '../../assets/images/Tiles.png')
+    this.load.image('coin', '../../assets/images/coin.png')
+    this.load.image('scoreholder', '../../assets/images/scoreholder.png')
+    this.load.image('sign', '../../assets/images/scoreholder.png')
+    this.load.image('gameover_bg', '../../assets/images/gameover_bg.png')
+    this.load.audiosprite('sfx', ['../../assets/sounds/sfx.mp3', '../../assets/sounds/sfx.ogg'], '../../assets/sounds/sfx.json')
   }
 
   render () {
