f30af8ae82af8f9096f30b2ad7e5a2f81825b26d
aurimas-darguzis <aurimas@darguzis.com>
2017-12-10 21:19:56 +0000

    update

---
+++ src/states/Level1.js
@@ -0,0 +1,29 @@
+import Player from '../prefabs/Player'
+import Mouse from '../prefabs/Mouse'
+import NumberBox from '../prefabs/NumberBox'
+
+export default class Level1 extends Phaser.State {
+  constructor () {
+    super()
+  }
+
+  create () {
+
+  }
+
+  update () {
+
+  }
+
+  collectCoin (playerRef, coinRef) {
+
+  }
+
+  hitDoor (playerRef, doorRef) {
+
+  }
+
+  hitEnemy (playerRef, enemyRef) {
+    
+  }
+}
